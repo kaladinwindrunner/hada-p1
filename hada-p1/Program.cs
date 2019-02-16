c863275061ec2bf32d1ce2d16c7904e3be86f2c2
unknown <avrs1@alu.ua.es>
2019-02-16 12:40:08 +0100

    Modified Program.cs file

--- hada-p1/Program.cs
+++ hada-p1/Program.cs
@@ -10,6 +10,32 @@ namespace hada_p1
     {
         static void Main(string[] args)
         {
+            char seguir;
+
+            do
+            {
+                string unidad = null;
+
+                do
+                {
+                    Console.WriteLine("¿Unidad de partida (minutos/segundos)? ");
+                    unidad = Console.ReadLine();
+                    if (unidad != "segundos" && unidad != "minutos") Console.WriteLine("¡Error: Unidad incorrecta!\n");
+                } while (unidad != "segundos" && unidad != "minutos");
+
+                Console.WriteLine("¿Cantidad? ");
+                double cantidad = double.Parse(Console.ReadLine());
+                if (unidad == "segundos") Console.WriteLine("{0} segundos son {1} minutos.\n", cantidad, HadaP1.Seconds2Minutes(cantidad));
+                else Console.WriteLine("{0} minutos son {1} segundos.\n", cantidad, HadaP1.Minutes2Seconds(cantidad));
+
+                do
+                {
+                    Console.WriteLine("¿Realizar más conversiones (s/n)? ");
+                    seguir = char.Parse(Console.ReadLine());
+                    if (seguir != 's' && seguir != 'n') Console.WriteLine("¡Error: Opción incorrecta!\n");
+                } while (seguir != 's' && seguir != 'n');
+
+            } while (seguir == 's');
         }
     }
 }
